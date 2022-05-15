389161730eb81edd9ed83f5f259930a3e36f947a
uvindu123 <uwimalasinghe@gmail.com>
2022-05-15 19:07:57 +0530

    commit

--- Electro-Grid/src/main/java/com/customerAPI.java
+++ Electro-Grid/src/main/java/com/customerAPI.java
@@ -19,6 +19,7 @@ public class customerAPI extends HttpServlet {
        
 	
 	 customer customerObj = new customer(); 
+	 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
